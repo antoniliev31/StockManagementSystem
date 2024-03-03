fb27e51f3e46eaab55f89ffafb3e28661300d31d
Anton Iliev <iliev.anton@gmail.com>
2024-03-03 13:58:10 +0200

    Artivle form repair new

--- StockManagementSystem.Web/wwwroot/js/jquey-overrade.js
+++ StockManagementSystem.Web/wwwroot/js/jquey-overrade.js
@@ -1,10 +1,10 @@
 ﻿$.validator.methods.range = function (value, element, param) {
-    var globalizedValue = parseFloat(value.replace(",", "."));
+    var globalizedValue = parseFloat(value.replace(",", ".")).toFixed(2);
     return this.optional(element) || (globalizedValue >= param[0] && globalizedValue <= param[1]);
 }
 
 $.validator.methods.number = function (value, element) {
-    var globalizedValue = parseFloat(value.replace(",", "."));
+    var globalizedValue = parseFloat(value.replace(",", ".")).toFixed(2);
     return this.optional(element) || /^-?(?:\d+|\d{1,3}(?:[\s\.,]\d{3})+)(?:[\.,]\d+)?$/.test(globalizedValue);
 }
 
